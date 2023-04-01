527a1005c0dc95b7cf64122b1b89553c026ba2d0
Tanish Garg <tanish.garg@icloud.com>
2023-04-01 11:13:18 -0500

    Create CSVFileConversion.py

---
+++ Tanish/CSVFileConversion.py
@@ -0,0 +1,5 @@
+import csv
+
+reader = csv.reader(open("C:\\Users\\tanis\\Downloads\\questionposts.csv", 'r', errors='replace'), delimiter=',')
+writer = csv.writer(open("C:\\Users\\tanis\\Downloads\\newquestionposts.csv", 'w', errors='replace', newline=''), delimiter=';')
+writer.writerows(reader)
